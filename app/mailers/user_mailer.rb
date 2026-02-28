7d66e68867f25aa50b312abc158763e60408469c
Jace Benson <638764+jacebenson@users.noreply.github.com>
2026-02-28 02:17:09 -0600

    updated email code

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,11 +3,58 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user
-    mail(to: @user.email, subject: "Welcome to news.jace.pro!")
+
+    # Render templates
+    html_content = render_to_string(template: "user_mailer/welcome_email", layout: "mailer", formats: [ :html ])
+    text_content = render_to_string(template: "user_mailer/welcome_email", layout: "mailer", formats: [ :text ])
+
+    # Send via Mailgun HTTP API
+    send_via_mailgun_api(
+      to: @user.email,
+      subject: "Welcome to news.jace.pro!",
+      html: html_content,
+      text: text_content
+    )
   end
 
   def password_reset(user)
     @user = user
-    mail(to: @user.email, subject: "Password reset instructions")
+
+    # Render templates
+    html_content = render_to_string(template: "user_mailer/password_reset", layout: "mailer", formats: [ :html ])
+    text_content = render_to_string(template: "user_mailer/password_reset", layout: "mailer", formats: [ :text ])
+
+    # Send via Mailgun HTTP API
+    send_via_mailgun_api(
+      to: @user.email,
+      subject: "Password reset instructions",
+      html: html_content,
+      text: text_content
+    )
+  end
+
+  private
+
+  def send_via_mailgun_api(to:, subject:, html:, text:)
+    response = HTTParty.post(
+      "https://api.mailgun.net/v3/news.jace.pro/messages",
+      basic_auth: {
+        username: "api",
+        password: ENV["MAILGUN_API_KEY"]
+      },
+      body: {
+        from: "news@jace.pro",
+        to: to,
+        subject: subject,
+        html: html,
+        text: text
+      }
+    )
+
+    unless response.success?
+      raise "Mailgun API error: #{response.body}"
+    end
+
+    response
   end
 end
